f1c37bd1bdd97ba2bec345ac67432c2d4d014af7
titomundo <tito.alberto.mundo@gmail.com>
2026-05-24 18:53:47 -0600

    task 2 and 3

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -7,6 +7,25 @@ interface Teacher {
 	[propName: string]: any;
 }
 
+interface Directors extends Teacher {
+	numberOfReports: number;
+}
+
+interface printTeacherFunction {
+	(firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) =>
+	`${firstName[0]}. ${lastName}`;
+
+const director1: Directors = {
+	firstName: "John",
+	lastName: "Doe",
+	location: "London",
+	fullTimeEmployee: true,
+	numberOfReports: 17,
+};
+
 const teacher3: Teacher = {
 	firstName: "John",
 	fullTimeEmployee: false,
@@ -16,3 +35,5 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+console.log(director1);
+console.log(printTeacher(teacher3.firstName, teacher3.lastName));
